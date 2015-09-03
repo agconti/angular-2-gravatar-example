04dcabe1112b3c5dec08e9b72d82112629d9358a
agconti <agc11d@gmail.com>
2015-09-03 17:43:45 -0400

    feat(src/main): basic concept completed

--- dist/all.js
+++ dist/all.js
@@ -3,6 +3,8 @@ System.register(['angular2/angular2'], function (_export) {
 
   var Component, View, bootstrap, GravatarProfile;
 
+  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
   function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
   return {
@@ -15,11 +17,29 @@ System.register(['angular2/angular2'], function (_export) {
       GravatarProfile = (function () {
         function GravatarProfile() {
           _classCallCheck(this, _GravatarProfile);
+
+          this.imageSrc = "https://placehold.it/350x150";
         }
+
+        _createClass(GravatarProfile, [{
+          key: 'doneTyping',
+          value: function doneTyping($event) {
+            if ($event.which === 13) {
+              this.getProfile($event.target.value);
+              $event.target.value = null;
+            }
+          }
+        }, {
+          key: 'getProfile',
+          value: function getProfile(val) {
+            console.log(val);
+            this.imageSrc = 'https://media3.giphy.com/media/Y8SqjWuohk8Rq/200_s.gif';
+          }
+        }]);
 
         var _GravatarProfile = GravatarProfile;
         GravatarProfile = View({
-          template: '\n    <img src="https://placehold.it/350x150" />\n    <input #email type="email"/>\n    '
+          template: '\n    <img src="{{imageSrc}}"/>\n    <input #email type="email" (keyup)="doneTyping($event)"/>\n    <button (click)="getProfile(email.value)">Get profile for {{email.value}}</button>\n    '
         })(GravatarProfile) || GravatarProfile;
         GravatarProfile = Component({
           selector: 'gravatar-profile'
--- src/main.js
+++ src/main.js
@@ -6,12 +6,27 @@ import {Component, View, bootstrap} from 'angular2/angular2'
 })
 @View({
   template: `
-    <img src="https://placehold.it/350x150" />
-    <input #email type="email"/>
+    <img src="{{imageSrc}}"/>
+    <input #email type="email" (keyup)="doneTyping($event)"/>
+    <button (click)="getProfile(email.value)">Get profile for {{email.value}}</button>
     `
 })
 class GravatarProfile {
+  constructor() {
+    this.imageSrc = "https://placehold.it/350x150"
+  }
 
+  doneTyping($event) {
+    if($event.which === 13) {
+      this.getProfile($event.target.value);
+      $event.target.value = null;
+    }
+  }
+
+  getProfile(val) {
+    console.log(val)
+    this.imageSrc = 'https://media3.giphy.com/media/Y8SqjWuohk8Rq/200_s.gif'
+  }
 }
 
 
